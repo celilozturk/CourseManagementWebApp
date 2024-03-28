cd53cfe7d06494b5dcd822f269e0edc9b8218653
Celil Ozturk <celil.ozturk@windowslive.com>
2024-03-28 22:02:25 +0300

    CandidatesController

---
+++ CourseManagementWebApp/Controllers/CandidatesController.cs
@@ -0,0 +1,24 @@
+﻿using CourseManagementWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseManagementWebApp.Controllers
+{
+    public class CandidatesController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult Apply()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Apply(Candidate model)
+        {
+            return View("Feedback",model);
+        }
+
+    }
+}
