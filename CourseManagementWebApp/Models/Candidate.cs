c2ff97bf41eb97b616cdb81c59ace65ffca40ae2
Celil Ozturk <celil.ozturk@windowslive.com>
2024-03-28 21:54:05 +0300

    Candidate Model

---
+++ CourseManagementWebApp/Models/Candidate.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseManagementWebApp.Models
+{
+    public class Candidate
+    {
+        [Required(ErrorMessage ="Email is required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName?.ToUpper()}";
+        public int Age { get; set; }
+        public List<Course> SelectedCourses { get; set; } 
+        public DateTime ApplyAt { get; set; }
+        public Candidate()
+        {
+            ApplyAt=DateTime.Now;
+        }
+    }
+}
