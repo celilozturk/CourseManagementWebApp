17c76ad5ad2cb7558cf8f55307bfc60f50f2ff09
Celil Ozturk <celil.ozturk@windowslive.com>
2024-03-28 22:26:33 +0300

    Apply Form, Model DataAnnotations

--- CourseManagementWebApp/Models/Candidate.cs
+++ CourseManagementWebApp/Models/Candidate.cs
@@ -1,13 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CourseManagementWebApp.Models
 {
     public class Candidate
     {
+        [DisplayName("Email Address")]
         [Required(ErrorMessage ="Email is required")]
         public string Email { get; set; }
+        [DisplayName("First Name")]
         [Required(ErrorMessage = "First Name is required")]
         public string FirstName { get; set; }
+        [DisplayName("Last Name")]
         [Required(ErrorMessage = "Last Name is required")]
         public string LastName { get; set; }
         public string FullName => $"{FirstName} {LastName?.ToUpper()}";
